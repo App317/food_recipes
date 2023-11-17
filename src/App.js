5ebf388df91843fa7483fcc0e22a501c99a67e69
App317 <101300095+App317@users.noreply.github.com>
2023-11-16 19:22:15 -0500

    1.0
    everything works as expected. might add some css changes later

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ function App() {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
 
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
     setRecipes(data.hits);
@@ -24,12 +25,17 @@ function App() {
 
   const updateSearch = (e) => {
     setSearch(e.target.value);
-    console.log(search);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
   };
 
   return (
     <div className="App">
-      <form className="search-form">
+      <form onSubmit={getSearch} className="search-form">
         <input
           className="search-bar"
           type="text"
@@ -40,14 +46,17 @@ function App() {
           Search
         </button>
       </form>
-      {recipes.map((recipe) => (
-        <Recipe
-          key={recipe.recipe.label}
-          title={recipe.recipe.label}
-          type={recipe.recipe.cuisineType}
-          image={recipe.recipe.image}
-        />
-      ))}
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            type={recipe.recipe.cuisineType}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import style from './recipe.module.css';
 
-const Recipe = ({ title, type, image }) => {
+const Recipe = ({ title, type, image, ingredients }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{type}</p>
-      <img src={image} alt="" />
+    <div className={style.recipe}>
+      <h1 className={style.title}>{title}</h1>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ul>
+      <p className={style.type}>{type}</p>
+      <img className={style.image} src={image} alt="" />
     </div>
   );
 };
